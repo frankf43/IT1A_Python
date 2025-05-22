12250a9e414eefce7996f1e4166477f13d3bdd91
frankf43 <filip.frank22@gmail.com>
2025-05-22 14:35:57 +0200

    Update Fisher_Yaytes_shuffle.py

--- Arr/Shuffle/Fisher_Yaytes_shuffle.py
+++ Arr/Shuffle/Fisher_Yaytes_shuffle.py
@@ -2,9 +2,9 @@ import random
 cards = list(range(1,33))
 n = len(cards)
 counter = 0
-while n:
+while n>1:
     n-=1
-    i = random.randint(0,n)
+    i = random.randint(0,n-1)
     cards[n],cards[i] = cards[i], cards[n]
     print(i,n,sep="||")
     print(cards)

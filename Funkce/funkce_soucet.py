dbe275776dfe3c9660432293d10c691e414ef8c6
frankf43 <frankf@kvd.zcu.cz>
2025-06-13 12:31:44 +0200

    funkce

--- Funkce/funkce_soucet.py
+++ Funkce/funkce_soucet.py
@@ -18,21 +18,21 @@ def main():
     clen1 = int(input("Zadejte první člen."))
     clen2 = int(input("Zadejte druhý člen."))
     print(soucet(clen1,clen2))
+    print(nasobeni(clen1, clen2))
 
 def soucet(a:int, b:int):
     soucet = a + b
     return soucet
 
-main()
+def nasobeni(clen1, clen2):
 
-def nasobeni():
-    clen1 = int(input("Zadejte člen 1: "))
-    clen2 = int(input("Zadejte člen 2"))
     vysledek = 0
     for i in range(clen2):
         vysledek = soucet(clen1, vysledek)
     return vysledek
-print(nasobeni())
+
+main()
+
 
 
 

0002cbce2166fbdc313e6b0b22e46fc3a3860fff
frankf43 <filip.frank22@gmail.com>
2026-02-03 09:51:58 +0100

    fstring

--- Porad/porad.py
+++ Porad/porad.py
@@ -2,6 +2,7 @@ cislo = int(input())
 soucet = cislo
 while cislo > 0:
     print("Zadali jste " + str(cislo) + ". Co dal?")
+    print(f"Zadali jste {cislo}. Co dál?")
     cislo = int(input())
     if cislo > 0:
         soucet += cislo

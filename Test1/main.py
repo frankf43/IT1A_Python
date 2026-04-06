c1b615cab0f75e0ffd566da18a77ba777fbcb3aa
frankf43 <filip.frank22@gmail.com>
2026-04-06 12:59:06 +0200

    details

--- Test1/main.py
+++ Test1/main.py
@@ -1,10 +1,10 @@
 while True:
-    choice = input("Zadejte tvar s kterým chcete pracovat. Obdélník/Čtverec/Trojúhelník").lower()
+    choice = input("Zadejte tvar s kterým chcete pracovat. Obdélník/Čtverec/Trojúhelník --> ").lower()
     match choice:
         case "obdélník":
-            a = int(input("Zadejte stranu A"))
-            b = int(input("Zadejte stranu B"))
-            operation = input("Přejete si obvod nebo obsah?")
+            a = int(input("Zadejte stranu A: "))
+            b = int(input("Zadejte stranu B: "))
+            operation = input("Přejete si obvod nebo obsah? ")
             match operation:
                 case "obvod":
                     print(f"Obvod vašeho obdélníku je {2*(a+b)}")
@@ -13,8 +13,8 @@ while True:
                 case _:
                     print("Neplatná volba.")
         case "čtverec":
-            a = int(input("Zadejte stranu A"))
-            operation = input("Přejete si obvod nebo obsah?")
+            a = int(input("Zadejte stranu A: "))
+            operation = input("Přejete si obvod nebo obsah? ")
             match operation:
                 case "obvod":
                     print(f"Obvod vašeho čtverce je {4*a}")
@@ -23,9 +23,9 @@ while True:
                 case _:
                     print("Neplatná volba.")
         case "trojúhelník":
-            a = int(input("Zadejte stranu A"))
-            b = int(input("Zadejte stranu B"))
-            c = int(input("Zadejte stranu C"))
+            a = int(input("Zadejte stranu A: "))
+            b = int(input("Zadejte stranu B: "))
+            c = int(input("Zadejte stranu C: "))
             if a + b > c and a + c > b and c + b > a:
                 print("Výborně trojúhelník lze sestavit!")
                 if a == b or b == c or c == a:
@@ -39,6 +39,6 @@ while True:
                 print("Bohužel, tento trojúhelník nelze sestavit.")
         case _:
             print("Neplatné zadání.")
-    if input("Přejete si pokračovat? Y/N").upper() != "Y":
+    if input("Přejete si pokračovat? Y/N --> ").upper() != "Y":
         print("Ukončuji program.")
         break
